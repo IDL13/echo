cb1d3e9ddce75adc5f39730c427f43619b4d7b2a
IDL13 <leonov.ilya.02@gmail.com>
2023-01-05 23:17:26 +0300

    post

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+type Card struct {
+	Number int `json:"number"`
+	Date   int `json:"date"`
+	CVV    int `json:"CVV"`
+}
 
 func main() {
 	fmt.Println("[SERVER STARTED]")
@@ -16,11 +25,35 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", handler)
+	e.GET("/", getHandler)
+	e.POST("/post", postHandler)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func handler(c echo.Context) error {
-	return c.String(http.StatusOK, "[SERVER STARTED]")
+func getHandler(c echo.Context) error {
+	err := c.String(http.StatusOK, "[SERVER STARTED]")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func postHandler(c echo.Context) error {
+	var card Card
+
+	defer c.Request().Body.Close()
+	b, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		log.Printf("Failed reading the request body: %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	err = json.Unmarshal(b, &card)
+	if err != nil {
+		log.Printf("Failed unmarsheling: %s", err)
+	}
+
+	log.Print("your card added")
+	return c.String(http.StatusOK, "")
 }
