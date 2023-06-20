63f62d62ca591664cc7596bab9895212f607fd46
IDL13 <leonov.ilya.02@gmail.com>
2023-06-20 23:54:39 +0300

    React frontend application

---
+++ www/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Form} from "./components/Form"
+
+function App() {
+  return (
+    <div className='bg-neutral-900 bg-auto'>
+    <h4 className = "w-96 mt-auto ml-auto mb-auto mr-auto text-white p-8 text-2xl">Card Holder</h4>
+    <Form />
+    </div>
+  )
+}
+
+export default App;
